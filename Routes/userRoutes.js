5f8f45f9cb0a68a2f438095aa117c1ad0c07b614
thanujadev26 <thanujapriyadarshane26@gmail.com>
2025-06-11 16:30:21 +0530

    authenticated user details fetched to the /profile route and details change functionality and password reset

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const protect = require('../Middleware/authMiddleware');
-const profileController = require("../Controllers/userController")
+const profileController = require("../Controllers/userController");
 
 router.get("/", protect, profileController.getSignedUserDetails)
-
+router.put("/", protect, profileController.updateSignedUserProfile)
+router.put("/password", protect, profileController.changePassword)
 
 module.exports = router;
